0a8bfc4877c4cbd4a845d76ed290eb49de9e0e61
George Dorobantu <gdorobantu@kinaxis.com>
2023-09-05 13:59:25 -0400

    build libraries only in Release

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -1,7 +1,6 @@
 hunter_config(SimpleLibrary
     VERSION 0.2
-    URL
-    "https://github.com/gdalex/hunter-test-lib/archive/0.2.tar.gz"
-    SHA1
-    dfc9f95ee9f3720e7bc94e1ecd8d67cfb524aaaf
+    URL "https://github.com/gdalex/hunter-test-lib/archive/0.2.tar.gz"
+    SHA1 dfc9f95ee9f3720e7bc94e1ecd8d67cfb524aaaf
+    CONFIGURATION_TYPES Release
 )
