79ede685e838250f3ec88ba89c0a82f86da592a0
George A. Dorobantu <gdorobantu@kinaxis.com>
2023-09-05 14:29:29 -0400

    commented out CONFIGURATION_TYPES

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -2,5 +2,5 @@ hunter_config(SimpleLibrary
     VERSION 0.2
     URL "https://github.com/gdalex/hunter-test-lib/archive/0.2.tar.gz"
     SHA1 dfc9f95ee9f3720e7bc94e1ecd8d67cfb524aaaf
-    CONFIGURATION_TYPES Release
+    #CONFIGURATION_TYPES "Release;Debug"
 )
