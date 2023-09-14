d7925c065124db51106b03d2ffca3372694d95e1
George Dorobantu <gdorobantu@kinaxis.com>
2023-09-14 13:58:40 -0400

    auth test

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -1,6 +1,8 @@
 hunter_config(SimpleLibrary
     VERSION 0.3
-    URL "https://github.com/gdalex/hunter-test-lib/archive/0.3.tar.gz"
+    URL "https://github.com/gdalex/hunter-test-lib/archive/refs/tags/0.3.zip"
     SHA1 d31ce498f28cde343deee90c4e9035928fa9c5fc
-    #CONFIGURATION_TYPES "Release;Debug"
 )
+
+include(hunter_protected_sources)
+hunter_protected_sources(SimpleLibrary)
